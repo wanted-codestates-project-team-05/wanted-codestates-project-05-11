847b442baf9dcaaf0bc13f0c506be0760f99e892
seungyeon.lee <dus3214@gmail.com>
2022-03-08 10:52:12 +0900

    style: 그래프 이미지 추가

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
+
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
     <HomePage />
+    <PentagonChart/>
+    <div class="bg-img"></div>
   </div>
 </template>
 
 <script>
 import HomePage from './components/home/HomePage.vue';
+import PentagonChart from './components/pentagon/PentagonChart.vue';
 
 export default {
   name: 'App',
   components: {
     HomePage,
+    PentagonChart,
   },
 };
 </script>
@@ -24,5 +29,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.bg-img {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  background-image: url('./assets/chart-bg.png');
+  background-size: 54px 54px;
+  width: 54px;
+  height: 54px;
+  border-radius: 50%;
+  z-index: 10;
 }
 </style>
