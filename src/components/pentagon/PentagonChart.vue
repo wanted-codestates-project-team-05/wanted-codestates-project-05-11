1a7a1ba60c68d2dbde7fd9b519c0841ddf8224a1
kyu <2066apple@naver.com>
2022-03-08 14:15:53 +0900

    fix: missing prop error fix and code refactory

--- src/components/pentagon/PentagonChart.vue
+++ src/components/pentagon/PentagonChart.vue
@@ -1,46 +1,31 @@
 <script>
-import { Radar, mixins } from 'vue-chartjs';
-import companyData from '../../assets/company.json';
-const { reactiveProp } = mixins;
-
-const data = companyData.KAKAO;
-
-const dataChart = [];
-for (let key in data) {
-  dataChart.push(data[key]);
-}
-export default {
-  extends: Radar,
-  mixins: [reactiveProp],
-  // data: () => {
-  //   company: company;
-  // },
-  props: {
-    chartdata: {
-      type: Object,
-      default: null,
+  import { Radar } from 'vue-chartjs'
+  
+  export default {
+    extends: Radar,
+    props: {
+      chartdata: {
+        type: Object,
+        default: null,
+      },
+      options: {
+        type: Object,
+        default: null
+      },
+      company: String,
     },
-    options: {
-      type: Object,
-      default: null,
+    watch: {
+      chartdata() {
+        this.render();
+      },
     },
-    company: String,
-  },
-  watch: {
-    chartdata() {
+    methods: {
+      render() {
+        this.renderChart(this.chartdata, this.options);
+      },
+    },
+    mounted() {
       this.render();
     },
-  },
-  methods: {
-    render() {
-      this.renderChart(this.chartdata, this.options);
-    },
-  },
-  mounted() {
-    this.render();
-    console.log(data);
-    console.log(this.company);
-    console.log(dataChart);
-  },
 };
 </script>
